f5d931948e7f0bc395c5eca1cbad61e9a1843485
Tersia Koetzee <tersiakoetzee@gmail.com>
2019-10-03 08:04:54 +0200

    test amended

--- test/greetings.test.js
+++ test/greetings.test.js
@@ -30,33 +30,6 @@ describe("The greetFactory", function () {
 
     })
 
-
-    it("should return Please Enter Name and Select Language, if no name was entered and no language was selected", function () {
-        let greeting = greetFactory();
-
-        greeting.setGreeting("", "")
-        assert.equal("Please Enter Name and Select Language", greeting.output())
-
-
-    })
-    it("should return No Name Entered, if no name was entered", function () {
-        let greeting = greetFactory();
-
-        greeting.setGreeting("")
-        assert.equal("No Name Entered", greeting.output())
-
-
-    })
-
-    it("if a name was entered but no language selected return, No Language Selected", function () {
-        let greeting = greetFactory();
-
-        greeting.setGreeting("Gill", "")
-        assert.equal("No language selected", greeting.output())
-
-
-    })
-
 });
 
 describe("The setCounter function", function () {
@@ -71,13 +44,5 @@ describe("The setCounter function", function () {
 
     })
 
-    it("only allow a name to be enterded once", function () {
-        let greeting = greetFactory();
 
-        greeting.storedNames("Mia")
-        greeting.storedNames("lia")
-        greeting.storedNames("tyla")
-        assert.equal(greeting.loadingName(3))
-
-    })
 });
